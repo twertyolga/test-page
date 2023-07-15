1d11af0d3a4314efcea733294833c1698df0f22d
Volha Buinitskaya <vbuinitskaya@evolution.com>
2023-07-15 11:59:15 +0400

    add routers

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom"
+
 import App from './App.tsx'
+import AdditionalPage from "./AdditionalPage.tsx";
 import './index.css'
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/test",
+        element: <AdditionalPage/>,
+    }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>,
 )
