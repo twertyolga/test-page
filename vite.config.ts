b14c503e48149ada89c04f40124a23dad578d3c6
Volha Buinitskaya <twertyolga@gmail.com>
2024-01-25 22:36:40 +0100

    Add sentry package

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => ({
-  plugins: [react()],
+  server: {https: true},
+  plugins: [basicSsl(),react()],
   base: "/test-page",
   mode,
   define: {
     __APP_VERSION__: JSON.stringify(process.env.VERSION),
+    __APP_ENVIRONMENT__: JSON.stringify(mode),
   },
 }))
